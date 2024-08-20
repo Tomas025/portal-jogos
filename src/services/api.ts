2aa91a35406e8afd2dc85cae414a3cd7777a1641
Tomas Braz <tomasbraz43@hotmail.com>
2024-08-20 10:05:02 -0300

    Corrigido o erro da tela de login

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,43 +1,36 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { jwtDecode } from 'jwt-decode';
 
 export const api = axios.create({
 	baseURL: 'http://localhost:3333'
 });
 
-// const myUser = Cookies.get('user');
-// const myDecodeUser = jwtDecode(myUser!);
-// let isRefreshing = false;
-
-// if (myUser) {
-// 	api.defaults.headers['Authorization'] = `Bearer ${myUser}`;
-// }
-
-// api.interceptors.request.use(
-// 	function (config) {
-// 		if (myUser) {
-// 			// alert('entrou');
-// 			if (myDecodeUser.exp! - Date.now() < 43200000) {
-// 				if (!isRefreshing) {
-// 					isRefreshing = true;
-// 					api.post('/auth/RefreshToken', { access_token: myUser })
-// 						.then((response) => {
-// 							Cookies.set('user', response.data.access_token, {
-// 								expires: 1
-// 							});
-// 							config.headers.Authorization = `Bearer ${response.data.access_token}`;
-// 						})
-// 						.catch((error) => {
-// 							console.log(error);
-// 						});
-// 				}
-// 			}
-// 		}
-// 		return config;
-// 	},
-// 	function (error) {
-// 		// Faça algo com erro da solicitação
-// 		return Promise.reject(error);
-// 	}
-// );
+api.interceptors.request.use(
+	function (config) {
+		const myUser = Cookies.get('user');
+		if (myUser) {
+			config.headers.Authorization = `Bearer ${myUser}`;
+			// alert('entrou');
+			// if (myDecodeUser.exp! - Date.now() < 43200000) {
+			// 	if (!isRefreshing) {
+			// 		isRefreshing = true;
+			// 		api.post('/auth/RefreshToken', { access_token: myUser })
+			// 			.then((response) => {
+			// 				Cookies.set('user', response.data.access_token, {
+			// 					expires: 1
+			// 				});
+			// 				config.headers.Authorization = `Bearer ${response.data.access_token}`;
+			// 			})
+			// 			.catch((error) => {
+			// 				console.log(error);
+			// 			});
+			// 	}
+			// }
+		}
+		return config;
+	},
+	function (error) {
+		// Faça algo com erro da solicitação
+		return Promise.reject(error);
+	}
+);
